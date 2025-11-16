253a94668344365d913519b2669b39163c52dae6
sudokushifter <sudokushifterz@gmail.com>
2025-11-16 18:57:13 +0300

    [MIND] - changed docker compose

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from src.bootstrap import bootstrap
 app = bootstrap().start_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
